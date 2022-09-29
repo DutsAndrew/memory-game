cca189814c6c973238a02ac800af689be0e31eb7
Andrew Dutson <Dutsandrew@gmail.com>
2022-09-28 20:34:45 -0600

    adjusted instructions memo

--- src/scripts/instructions.js
+++ src/scripts/instructions.js
@@ -1,5 +1,5 @@
 const showInstructions = () => {
-  alert('To play the Memory Game: 1) Select any card to update your score, 2) Try to remember and select all 8 cards, 3) If your score reaches 8 you have beat the game. Have fun! :)');
+  alert('To play the Memory Game: 1) Click on any card to update your score, 2) You cannot click the same card twice, doing so will reset your score to 0, 3) Try to remember all 8 cards and click each card only once, 4) If your score reaches 8 you have beat the game. Have fun! :)');
 }
 
 export default showInstructions;
